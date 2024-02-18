5f26b438734754829303796f96e4b5d55abd7329
AlbertPeterLunaGuzman <127205150+AlbertPeterLunaGuzman@users.noreply.github.com>
2024-02-18 21:08:39 +0800

    Update MainActivity.java

--- pukinangNGINANATAPOSDEN/MainActivity.java
+++ pukinangNGINANATAPOSDEN/MainActivity.java
@@ -5,10 +5,10 @@ import androidx.cardview.widget.CardView;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,21 +17,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //tawagin mo sa main method
         executeLogicDamingAlamHays();
     }
 
+    //tawagin mo sa main method
     public void executeLogicDamingAlamHays(){
+        //Palitan mo nalang yung mga id ng Id ng cards mo
         CardView firstCard = findViewById(R.id.firstCard);
         CardView secondCard = findViewById(R.id.secondCard);
         CardView thirdCard = findViewById(R.id.thirdCard);
 
+        //tas lagay mo dito yung mga card names
         showFirstDialog(firstCard);
         showSecondDialog(secondCard);
         showThirdDialog(thirdCard);
     }
+    // tawagin mo sa "executeLogicDamingAlamHays"
     public void showFirstDialog(CardView card){
         Dialog firstDialog = new Dialog(MainActivity.this);
+        //Palitan mo nalang yung R.layout.nameNungLayoutMo
         firstDialog.setContentView(R.layout.firstdialogbox);
+        //Palitan mo alang yung R.drawable.nameNungBackgroundMo sa drawables
         firstDialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.background));
         firstDialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
@@ -39,9 +46,22 @@ public class MainActivity extends AppCompatActivity {
         card.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                // palitan muna yung background card color
+                card.setCardBackgroundColor(getResources().getColor(R.color.black));
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Ibalik yung background card color
+                        card.setCardBackgroundColor(getResources().getColor(R.color.white));
+                    }
+                }, 100); // 100 = yung oras ng tagal bago ibalik sa dati
+
                 firstDialog.show();
             }
         });
+
+        //palitan mo nalang yung button id nung nandun sa dialog box mo
         Button firstButton = firstDialog.findViewById(R.id.firstDialogButton);
         firstButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -49,6 +69,9 @@ public class MainActivity extends AppCompatActivity {
                 firstDialog.dismiss();
             }
         });
+
+        //copy paste mo nalang ng dalawang beses pa
+
     }
 
     public void showSecondDialog(CardView card){
@@ -58,9 +81,19 @@ public class MainActivity extends AppCompatActivity {
         secondDialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
 
+
         card.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                card.setCardBackgroundColor(getResources().getColor(R.color.lightBlue));
+
+                new Handler().postDelayed(new Runnable(){
+                    @Override
+                    public void run(){
+                        card.setCardBackgroundColor(getResources().getColor(R.color.white));
+                    }
+                },100);
+
                 secondDialog.show();
             }
         });
@@ -84,6 +117,14 @@ public class MainActivity extends AppCompatActivity {
         card.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                card.setCardBackgroundColor(getResources().getColor(R.color.lightGreen));
+
+                new Handler().postDelayed(new Runnable(){
+                    @Override
+                    public void run(){
+                        card.setCardBackgroundColor(getResources().getColor(R.color.white));
+                    }
+                },100);
                 thirdDialog.show();
             }
         });
